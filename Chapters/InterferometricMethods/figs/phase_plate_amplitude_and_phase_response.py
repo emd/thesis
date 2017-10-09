32a9e94a256e8b6e515bdb6698b55d80e673a75b
Evan Davis <emd@mit.edu>
2017-10-08 19:23:12 -0700

    Updated plots of PCI amplitude and phase responses

--- Chapters/InterferometricMethods/figs/phase_plate_amplitude_and_phase_response.py
+++ Chapters/InterferometricMethods/figs/phase_plate_amplitude_and_phase_response.py
@@ -4,25 +4,23 @@ from wavenumber_response import PhaseContrastImaging
 
 
 # Optical parameters of PCI system
-kg = 2.  # kg = 2 / w0 is the diffraction limit
-M = 0.5  # typical...
-
-# Detector size
-kfsv_desired = 30.                # [kfsv_desired] = 1 / [w0]
-s = 2 * np.pi * M / kfsv_desired  # gives s ~ 0.1 * (M * w0), which is typical
-print 'detector element size in image plane, s = %f w0\n' % s
+kg = 2.  # [kg] = 1 / w0;  kg = 2 is the diffraction limit
+M = 0.5  # [M] = unitless; typical value...
 
 # Computational grid
-kmax = 5 * kg
-dk = 0.01
-x_I_max = 1.
-dx_I = 0.01
+kmax = 5 * kg       # [kmax] = [kg]
+dk = 0.01           # [dk] = [kg]
+
+xmax = 1.5          # [xmax] = w0
+dx = 0.01           # [dx] = w0
+xmax_I = xmax * M   # [xmax_I] = M * w0
+dx_I = dx * M       # [dx_I] = M * w0
 
 
 if __name__ == '__main__':
-    pci = PhaseContrastImaging(kg=kg, M=M, s=s)
-    pci.applyTo(kmax=kmax, dk=dk, x_I_max=x_I_max, dx_I=dx_I)
+    pci = PhaseContrastImaging(kg=kg, M=M, s=1)
+    pci.applyTo(kmax=kmax, dk=dk, x_I_max=xmax_I, dx_I=dx_I)
 
-    fig, axes = pci.plotTpp(interpolation='nearest')
+    fig, axes = pci.plotResponse(interpolation='nearest')
 
     plt.show()
