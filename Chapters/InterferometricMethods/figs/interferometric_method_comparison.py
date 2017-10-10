7f024a6ec58f78e6f34813335888c57278261c6b
Evan Davis <emd@mit.edu>
2017-10-09 23:00:46 -0700

    Plot is now a "true" comparison of transfer functions

--- Chapters/InterferometricMethods/figs/interferometric_method_comparison.py
+++ Chapters/InterferometricMethods/figs/interferometric_method_comparison.py
@@ -27,37 +27,66 @@ if __name__ == '__main__':
     pci = PhaseContrastImaging(kg=kg, M=M, s=s)
     int_het = HeterodyneInterferometer(M=M, s=s)
     int_hom_opt = HomodyneInterferometer(M=M, s=s, dphi=(np.pi / 2))
-    # int_hom_half_opt = HomodyneInterferometer(M=M, s=s, dphi=(np.pi / 4))
-    # int_hom_quart_opt = HomodyneInterferometer(M=M, s=s, dphi=(np.pi / 8))
 
     # Compute wavenumber response of each interferometric method
     pci.applyTo(kmax=kmax, dk=dk, x_I_max=x_I_max, dx_I=dx_I)
     int_het.applyTo(kmax=kmax, dk=dk)
     int_hom_opt.applyTo(kmax=kmax, dk=dk)
-    # int_hom_half_opt.applyTo(kmax=int_hom_half_opt.kfsv, dk=dk)
-    # int_hom_quart_opt.applyTo(kmax=int_hom_quart_opt.kfsv, dk=dk)
 
     # Determine index corresponding to PCI beam's center point
     # (in contrast, the homodyne and heterodyne interferometer
     # transfer functions are *independent* of the spatial coordinate)
     beam_center = len(pci.x_I) // 2
 
+    # Also, PCI transfer function only defined for |k| >= kg
+    hi_kind_pos = np.where(pci.k >= kg)[0]
+    hi_kind_neg = np.where(pci.k <= -kg)[0]
+    lo_kind = np.where(np.abs(pci.k) < kg)[0]
+
     # Color-blind proof colors
     pci_col, het_col, hom_col = get_distinct(3)
 
     fig = plt.figure()
 
-    plt.semilogy(pci.k, np.abs(pci.A_pci[:, beam_center]),
-                 c=pci_col, linewidth=linewidth)
-    plt.semilogy(int_het.k, int_het.Thet,
-                 c=het_col, linewidth=linewidth)
-    plt.semilogy(int_hom_opt.k, int_hom_opt.Thom,
-                 c=hom_col, linewidth=linewidth, linestyle='-')
-    # plt.semilogy(int_hom_half_opt.k, int_hom_half_opt.Thom,
-    #              c=hom_col, linewidth=linewidth, linestyle='--')
-    # plt.semilogy(int_hom_quart_opt.k, int_hom_quart_opt.Thom,
-    #              c=hom_col, linewidth=linewidth, linestyle=':')
+    # PCI transfer function
+    plt.semilogy(
+        pci.k[hi_kind_neg],
+        np.abs(pci.A_pci[hi_kind_neg, :][:, beam_center]),
+        c=pci_col,
+        linewidth=linewidth,
+        label='PCI')
+    plt.semilogy(
+        pci.k[hi_kind_pos],
+        np.abs(pci.A_pci[hi_kind_pos, :][:, beam_center]),
+        c=pci_col,
+        linewidth=linewidth)
 
+    # Heterodyne interferometer transfer function
+    plt.semilogy(
+        int_het.k,
+        int_het.Thet,
+        c=het_col,
+        linewidth=linewidth,
+        label='heterodyne int.')
+
+    # Homodyne interferometer transfer function
+    plt.semilogy(
+        int_hom_opt.k,
+        int_hom_opt.Thom,
+        c=hom_col,
+        linewidth=linewidth,
+        linestyle='-',
+        label='homodyne int.')
+
+    # PCI low-k amplitude response
+    plt.semilogy(
+        pci.k[lo_kind],
+        np.abs(pci.A_pci[lo_kind, :][:, beam_center]),
+        c=pci_col,
+        linewidth=linewidth,
+        linestyle='-.')
+
+    # PCI low-k cutoff
     plt.axvline(x=pci.kg, c='k', linewidth=linewidth, linestyle='--')
     plt.axvline(x=-pci.kg, c='k', linewidth=linewidth,  linestyle='--')
 
@@ -68,10 +97,6 @@ if __name__ == '__main__':
     plt.ylabel('$T(k)$', fontsize=fontsize)
     # plt.title('interferometric method transfer functions')
 
-    plt.legend([
-        'PCI (x = 0)',
-        'heterodyne int.',
-        'homodyne int.'],  # $\phi_R - \\bar{\phi} = \pi / 2$'],
-        loc='upper right')
+    plt.legend(loc='upper right')
 
     plt.show()
