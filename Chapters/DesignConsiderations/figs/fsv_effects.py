6fac976c6568270bf8b6551de407a4a06e723433
emd <emd@mit.edu>
2017-10-16 18:06:25 -0700

    Finite sampling-volume discussion

---
+++ Chapters/DesignConsiderations/figs/fsv_effects.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Plotting parameters
+kmax = 3    # [kmax] = k_{fsv}
+dk = 0.01   # [dk] = [kmax]
+linewidth = 2
+fontsize = 12
+
+
+if __name__ == '__main__':
+    k = np.arange(-kmax, kmax + dk, dk)
+
+    plt.figure()
+
+    plt.plot(k, 0.25 * np.sinc(k),
+             label='FSV included',
+             c='C0', linestyle='-', linewidth=linewidth)
+    plt.plot(k, 0.25 * np.ones(len(k)),
+             label='no FSV',
+             c='C0', linestyle='--', linewidth=linewidth)
+    plt.axhline(0, c='k')
+
+    plt.xlim([-kmax, kmax])
+    plt.xlabel('$k \; [k_{\mathrm{fsv}}]$', fontsize=fontsize)
+    plt.ylabel('$T_{\mathrm{het}}(k)$ [unitless]', fontsize=fontsize)
+    plt.legend(loc='best')
+
+    plt.show()
