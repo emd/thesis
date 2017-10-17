79940f72428b8fe8bc7e9df238618106eddf123f
emd <emd@mit.edu>
2017-10-16 18:28:42 -0700

    Polishing geometric considerations section

--- Chapters/DesignConsiderations/figs/fsv_effects.py
+++ Chapters/DesignConsiderations/figs/fsv_effects.py
@@ -15,16 +15,20 @@ if __name__ == '__main__':
     plt.figure()
 
     plt.plot(k, 0.25 * np.sinc(k),
-             label='FSV included',
+             # label='FSV included',
+             label='finite sampling volume',
              c='C0', linestyle='-', linewidth=linewidth)
     plt.plot(k, 0.25 * np.ones(len(k)),
-             label='no FSV',
+             # label='no FSV',
+             label='no finite sampling volume',
              c='C0', linestyle='--', linewidth=linewidth)
     plt.axhline(0, c='k')
 
     plt.xlim([-kmax, kmax])
+    plt.ylim([-0.1, 0.35])
     plt.xlabel('$k \; [k_{\mathrm{fsv}}]$', fontsize=fontsize)
     plt.ylabel('$T_{\mathrm{het}}(k)$ [unitless]', fontsize=fontsize)
-    plt.legend(loc='best')
+    # plt.legend(loc='best')
+    plt.legend(loc='upper right')
 
     plt.show()
