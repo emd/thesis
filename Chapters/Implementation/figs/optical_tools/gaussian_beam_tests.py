434a18171067807cc0d94531d4c367c09041a3ae
Evan Davis <emd@mit.edu>
2017-10-25 12:48:09 -0700

    Test of Gaussian beam focusing

--- Chapters/Implementation/figs/optical_tools/gaussian_beam_tests.py
+++ Chapters/Implementation/figs/optical_tools/gaussian_beam_tests.py
@@ -2,7 +2,7 @@ from nose import tools
 import numpy as np
 
 from gaussian_beam import GaussianBeam, w, R
-from ABCD_matrices import prop
+from ABCD_matrices import prop, lens
 
 
 def test_GaussianBeam__init__():
@@ -79,5 +79,26 @@ def test_GaussianBeam_propagation():
     g = g0.applyABCD(prop(z))
     tools.assert_almost_equal(g.w, w(z, w0, g0.zR))
     tools.assert_almost_equal(g.R, R(z, g0.zR))
+
+    return
+
+
+def test_GaussianBeam_propagation_and_focusing():
+    # 1/e E waist of unity; with wavelength of `pi`, this should
+    # result in a Rayleigh range of unity
+    w0 = 1.
+    R0 = np.inf
+    wavelength = np.pi
+    g0 = GaussianBeam(None, w=w0, R=R0, wavelength=wavelength)
+
+    # An optic with focal length `f` decreases a Gaussian beam's
+    # radius of curvature `R` by `f`. Thus, if a Gaussian beam
+    # with radius of curvature `R` strikes a focusing optic
+    # with focal length `f = R`, we expect the emerging beam
+    # to be at its waist.
+    z = 1
+    g1 = g0.applyABCD(prop(z))
+    g2 = g1.applyABCD(lens(g1.R))
+    tools.assert_almost_equal(g2.z, 0)
 
     return
